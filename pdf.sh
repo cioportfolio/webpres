9b3dc3480db80e07f66dd375c1cfb058faac262e
Richard Barton <1richard.barton@gmail.com>
2019-05-30 21:59:38 +0100

    Remove export command from pdf script

--- pdf.sh
+++ pdf.sh
@@ -4,7 +4,6 @@ pushd /vagrant
 
 rm -rf pdf || true
 mkdir pdf
-node_modules/ejs-cli/bin/ejs-cli views/template.ejs -O '...'$1'...' > export/$1.html
 `npm bin`/decktape http://localhost:8000/$1 pdf/$1.pdf
 
 popd
